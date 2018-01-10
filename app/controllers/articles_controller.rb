5bc088ff05bf26ae4430a0dec990938fa3bb8660
saimun <imran.saimun@bdipo.com>
2018-01-10 11:57:03 +0600

    destroy and homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,13 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was seccessfully deleted"
+    redirect_to articles_path
+
+  end
 
 
   private
